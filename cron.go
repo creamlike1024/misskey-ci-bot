e1431440121fa15088f7c3d383532f8d8ac20594
creamlike1024 <creamlike1024@outlook.com>
2023-04-23 10:57:15 +0800

    fix cronjob

--- cron.go
+++ cron.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/robfig/cron/v3"
+	"github.com/go-co-op/gocron"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 var CronJobSlice []CronJob
@@ -15,56 +15,34 @@ type CronJob struct {
 }
 
 func CronJobRunner() {
-	/*s := gocron.NewScheduler(time.Local)
+	s := gocron.NewScheduler(time.Local)
 	s.TagsUnique()
 	for _, job := range CronJobSlice {
 		switch job.action {
 		case "reindex":
 			s.Tag(job.action).Cron(job.cron).Do(func() {
 				err := DbReindex()
-				CronJobRepoter(err, job.action)
+				CronJobRepoter(err, "reindex")
 			})
 		case "backup":
 			s.Tag(job.action).Cron(job.cron).Do(func() {
 				err := DbBackup(false)
-				CronJobRepoter(err, job.action)
+				CronJobRepoter(err, "backup")
 			})
-		case "analyse":
+		case "analyze":
 			s.Tag(job.action).Cron(job.cron).Do(func() {
 				err := DbAnalyze()
-				CronJobRepoter(err, job.action)
+				CronJobRepoter(err, "analyze")
 			})
 		}
 	}
 	s.StartBlocking()
-	*/
-	c := cron.New()
-	for _, job := range CronJobSlice {
-		switch job.action {
-		case "reindex":
-			c.AddFunc(job.cron, func() {
-				err := DbReindex()
-				CronJobRepoter(err, job.action)
-			})
-		case "backup":
-			c.AddFunc(job.cron, func() {
-				err := DbBackup(false)
-				CronJobRepoter(err, job.action)
-			})
-		case "analyse":
-			c.AddFunc(job.cron, func() {
-				err := DbAnalyze()
-				CronJobRepoter(err, job.action)
-			})
-		}
-	}
-	c.Start()
 }
 
 func CronJobRepoter(err error, action string) {
 	// 根据任务执行结果发送通知
 	if err != nil {
-		log.Infof("CronJob: %s excuted", action)
+		log.Infof("CronJob: %s excuted failed.", action)
 		IndependentNotification <- fmt.Sprintf("定时任务：%s 执行失败", action)
 		return
 	}
